86a5da2d87e5621a9fd8e91cdd91f84d85bd15cf
Bluegnarl <bluegnarl@gmail.com>
2024-03-14 23:00:12 +0100

    fixa

--- api/src/templates/home_template/home.php
+++ api/src/templates/home_template/home.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./default.css">
-    <link rel="stylesheet" href="./home.css">
+    <link rel="stylesheet" href="./home.module.css">
     <title>Azureblade</title>
 </head>
 <body>
