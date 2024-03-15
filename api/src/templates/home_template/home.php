fef2724110e6698c85ce166c5153b5af552da010
Bluegnarl <bluegnarl@gmail.com>
2024-03-15 08:44:50 +0100

    fix vercel 516516546541651

--- api/src/templates/home_template/home.php
+++ api/src/templates/home_template/home.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../default.css">
-    <link rel="stylesheet" href="../src/templates/home_template/home.module.css">
+    <link rel="stylesheet" href="./src/templates/home_template/home.module.css">
     <title>Azureblade</title>
 </head>
 <body>
