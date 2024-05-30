1d32909af115bb07e93c7569e72fca979ba451b9
Bluegnarl <bluegnarl@gmail.com>
2024-05-30 09:17:19 +0200

    Modal Help new

--- public/assets/partials/modal_help.php
+++ public/assets/partials/modal_help.php
@@ -1,58 +1,18 @@
 <div class="modal modal-help">
-    <div class="labels-container">
-        <div class="label-container label-container-title">
-            <div class="icon" style="background-image: url(/assets/img/help.svg);"></div>
-            <p class="label-higher" style="color: var(--on-surface-1);">Help</p>
-        </div>
-        <div class="icon-container buttons-clickable" onclick="closeOverlay()">
-            <div class="icon" style="background-image: url(/assets/img/close.svg);"></div>
-        </div>
+    <div class="modal-header">
+        <p class="label">Mechanics</p>
+        <div class="icon" style="background-image: url(/assets/img/close.svg);"></div>
     </div>
-    <div class="cards">
-        <div class="card">
-            <div class="label-container buttons-clickable-exceptions-second" onclick="helpCase(1)">
-                <div class="icon" style="background-image: url(/assets/img/location.svg);"></div>
-                <p class="label" style="color: var(--on-surface-1);">Dilemmas</p>
-                <div class="icon arrow-icon" style="background-image: url(/assets/img/arrow.svg);"></div>
-            </div>
-            <div class="help-case help-case-1">
-                <div class="card-image"></div>
-                <div class="text-container">
-                    <p class="body-text scrollbar">
-                        You will face time limited dilemmas to convince your interlocutor. If you fail to respond or make a wrong choice, the Anger Meter will increase and it will potentially lead you to death once it hits his maximum.
-                    </p>
-                </div>
-            </div>
+    <div class="divider"></div>
+    <div class="modal-menu">
+        
+    </div>
+    <div class="modal-subject">
+        <div class="modal-img">
+            <img src="/assets/img/card-image.png" alt="">
         </div>
-        <div class="card">
-            <div class="label-container buttons-clickable-exceptions-second" onclick="helpCase(2)">
-                <div class="icon" style="background-image: url(/assets/img/location.svg);"></div>
-                <p class="label" style="color: var(--on-surface-1);">Choices</p>
-                <div class="icon arrow-icon" style="background-image: url(/assets/img/arrow.svg);"></div>
-            </div>
-            <div class="help-case help-case-2">
-                <div class="card-image"></div>
-                <div class="text-container">
-                    <p class="body-text scrollbar">
-                        During dialogues, you will have to make choices which are decisive to the story. Take care, bad choices can lead you to death.
-                    </p>
-                </div>
-            </div>
-        </div>
-        <div class="card">
-            <div class="label-container buttons-clickable-exceptions-second" onclick="helpCase(3)">
-                <div class="icon" style="background-image: url(/assets/img/location.svg);"></div>
-                <p class="label" style="color: var(--on-surface-1);">QTE</p>
-                <div class="icon arrow-icon" style="background-image: url(/assets/img/arrow.svg);"></div>
-            </div>
-            <div class="help-case help-case-3">
-                <div class="card-image"></div>
-                <div class="text-container">
-                    <p class="body-text scrollbar" style="overflow-y: auto;">
-                        At some point in the game, you will face Quick Time Event (QTE) which means that you will have to press a button in a short time, otherwise, you will be disadvantaged and it can lead you to death.
-                    </p>
-                </div>
-            </div>
-        </div>
+        <p class="body-text">
+        You will face time limited dilemmas to convince your interlocutor. If you fail to respond or make a wrong choice, the Anger Meter will increase and it will potentially lead you to death once it hits his maximum.
+        </p>
     </div>
 </div>
