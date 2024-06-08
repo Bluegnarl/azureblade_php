b36eff5f6736d16c77cf79a4e232740093478e8c
Bluegnarl <bluegnarl@gmail.com>
2024-06-08 19:22:45 +0200

    fix

--- public/assets/actions/cookies.php
+++ public/assets/actions/cookies.php
@@ -1,7 +1,10 @@
 <?php
 
-$w = $_COOKIE['way'];
-$i = $_COOKIE['scene'];
+if (isset($_COOKIE['way']) && isset($_COOKIE['scene'])) {
+    $w = $_COOKIE['way'];
+    $i = $_COOKIE['scene'];
+}
+
 if (isset($_COOKIE['endings'])) {
     $cookie_endings = json_decode($_COOKIE['endings']) ?? null;
 }
