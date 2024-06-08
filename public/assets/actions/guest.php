06c8f85cd6e9dc056417318fbda7744efa968dfd
Bluegnarl <bluegnarl@gmail.com>
2024-06-08 10:52:24 +0200

    test

--- public/assets/actions/guest.php
+++ public/assets/actions/guest.php
@@ -4,6 +4,9 @@ function guest() {
     if ($_GET['guest'] == "new") {
         setcookie("way", 0, time() + 360000, "/");
         setcookie("scene", 0, time() + 360000, "/");
+        if(!isset($_GET['endings'])) {
+            setcookie("endings", json_encode([]), time() + 360000, "/");
+        }
     }
 
     header('Location: /?page=game');
