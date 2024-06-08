33e004fcaa4820d22073b4b7ea8a6365bdd733d4
Bluegnarl <bluegnarl@gmail.com>
2024-06-08 18:54:21 +0200

    Cookie fix

--- public/assets/actions/guest.php
+++ public/assets/actions/guest.php
@@ -4,7 +4,7 @@ function guest() {
     if ($_GET['guest'] == "new") {
         setcookie("way", 0, time() + 360000, "/");
         setcookie("scene", 0, time() + 360000, "/");
-        if(!isset($_GET['endings'])) {
+        if(!isset($_COOKIE['endings'])) {
             setcookie("endings", json_encode([]), time() + 360000, "/");
         }
     }
